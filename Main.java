5ada93cde9eded93a7d60ac3502ff76bf4b2b91a
pratikshabathe <pratikshabathe11@gmail.com>
2022-11-25 15:52:58 +0530

    Create new Branch

--- Main.java
+++ Main.java
@@ -6,7 +6,6 @@ public class Main {
 	 public static void main(String[] args) {
 	        System.out.println("Welcome to linkedList DataStructure Program");
 	        Scanner scanner = new Scanner(System.in);
-	        System.out.printf(" add the data at the start" ,  Operations.addDataAtStart()); 
-	      
+	        System.out.printf(" add the data at the End", Operations.addDataAtEnd());
 	    }
 }
--- Operations.java
+++ Operations.java
@@ -2,12 +2,14 @@ package com.blz.linkedlist;
 
 public class Operations {
 
-	 public static Object[] addDataAtStart() {
-	        LinkedList linkedList = new LinkedList();
-	        linkedList.push(70);
-	        linkedList.push(30);
-	        linkedList.push(56);
-	        linkedList.print();
-			return null;
-	    }
+	// Created a method to add data at end.
+	
+	public static Object[] addDataAtEnd() {
+		LinkedList linkedList = new LinkedList();
+	    linkedList.append(56);
+	    linkedList.append(30);
+	    linkedList.append(70);
+	    linkedList.print();
+		return null;
+	}
 }
