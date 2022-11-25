c7e5f5f79a820532abfeae219361be695c96920d
pratikshabathe <pratikshabathe11@gmail.com>
2022-11-25 15:01:00 +0530

    Added : new beanch create

---
+++ LinkedList.java
@@ -0,0 +1,25 @@
+package com.blz.linkedlist;
+
+public class LinkedList {
+
+	public static void display() {
+		System.out.println("Welcome to data structure problems using java Generics");
+	}
+	
+	public static void main(String[] args) {
+		display();
+		
+		Node firstNode = new Node(56);
+		Node secondNode = new Node(30);
+		Node thirdNode = new Node(70);
+		Node head = firstNode;
+		firstNode.next = secondNode;
+		secondNode.next = thirdNode;
+		
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
+}
